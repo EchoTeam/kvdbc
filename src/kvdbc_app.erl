54e9574f11be97507d3c726c118f33f51fd7cc2e
snaky <snaky@aboutecho.com>
2013-07-02 16:09:07 +0400

    Generate module 'kvdbc_cfg'

--- src/kvdbc_app.erl
+++ src/kvdbc_app.erl
@@ -12,7 +12,77 @@
 %% ===================================================================
 
 start(_StartType, _StartArgs) ->
+    {module, kvdbc_cfg} = load_config(),
     kvdbc_sup:start_link().
 
 stop(_State) ->
     ok.
+    
+load_config() ->
+    {ok, BInstances} = application:get_env(kvdbc, backend_instances),
+    Spec = config_mod_spec(BInstances),
+    mod_gen:go(Spec).
+
+config_mod_spec(Config) ->
+    Vals = lists:umerge([
+        [
+            lists:flatten(io_lib:format("backend_val(~s, ~s) -> ~p", [I, K, V]))
+            || {K, V} <- Opts
+        ]
+        || {I, Opts} <- Config
+    ]),
+    [
+        ["-module(kvdbc_cfg).\n"],
+        ["-export([backend_val/2]).\n"],
+        [string:join(Vals, ";\n"), "."]
+    ].
+
+
+%% ===================================================================
+%% Tests
+%% ===================================================================
+
+-ifdef(TEST).
+-include_lib("eunit/include/eunit.hrl").
+
+config_mod_spec_test_() ->
+    [
+        fun() ->
+            Instances = [
+              {instance1, [
+                {callback_module, module1},
+                {process_name, process1},
+                {config, [
+                  {k1, v1}
+                ]}
+              ]},
+              {instance2, [
+                {callback_module, module2},
+                {process_name, process2},
+                {config, [
+                  {k2, v2}
+                ]}
+              ]}
+            ],
+            ResultSpec = config_mod_spec(Instances),
+            ExpectedSpec = 
+                <<"-module(kvdbc_cfg).\n"
+                "-export([backend_val/2]).\n"
+                "backend_val(instance1, callback_module) -> module1;\n"
+                "backend_val(instance1, process_name) -> process1;\n"
+                "backend_val(instance1, config) -> [{k1,v1}];\n"
+                "backend_val(instance2, callback_module) -> module2;\n"
+                "backend_val(instance2, process_name) -> process2;\n"
+                "backend_val(instance2, config) -> [{k2,v2}].">>,
+
+            ?debugFmt("ExpectedSpec:~n~s", [ExpectedSpec]),
+            ?debugFmt("ResultSpec:~n~s", [ResultSpec]),
+
+            ?assertEqual(ExpectedSpec, iolist_to_binary(ResultSpec)),
+            ?assertEqual({module, kvdbc_cfg}, mod_gen:go(ResultSpec)),
+            ?assertEqual(module1,
+                kvdbc_cfg:backend_val(instance1, callback_module))
+        end
+    ].
+
+-endif.
