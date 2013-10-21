b1fcf1e5e0bc256200a9977aa584b52e613ed0dd
Roslyakov Ilya <roslyakov@aboutecho.com>
2013-10-21 09:50:58 +0400

    import riakc_cluster types

--- src/kvdbc_riak_backend.erl
+++ src/kvdbc_riak_backend.erl
@@ -15,11 +15,7 @@
     list_keys/3
 ]).
 
--type error() :: {'error', 'notfound' | {'riak', riakc_cluster:errors()}}.
--type process_name() :: riakc_cluster:cluster_name().
--type table() :: riakc_cluster:table().
--type key() :: riakc_cluster:key().
--type value() :: riakc_cluster:value().
+-include_lib("riakc_cluster/include/riakc_cluster_types.hrl").
 
 -define(HANDLER_MODULE, 'riakc_cluster').
 
@@ -27,22 +23,22 @@ start_link(BackendName, ProcessName) ->
     Config = kvdbc_cfg:backend_val(BackendName, config),
     riakc_cluster:start_link(ProcessName, Config).
 
--spec put(_BackendName :: term(), ProcessName :: process_name(), Table :: table(), Key :: key(), Value :: value()) -> error() | 'ok'.
+-spec put(_BackendName :: term(), ProcessName :: cluster_name(), Table :: table(), Key :: key(), Value :: value()) -> error() | 'ok'.
 put(_BackendName, ProcessName, Table, Key, Value) ->
     riakc_cluster:put(ProcessName, Table, Key, Value, [{w, 2}]).
 
--spec get(_BackendName :: term(), ProcessName :: process_name(), Table :: table(), Key :: key()) -> error() | {'ok', value()}.
+-spec get(_BackendName :: term(), ProcessName :: cluster_name(), Table :: table(), Key :: key()) -> error() | {'ok', value()}.
 get(_BackendName, ProcessName, Table, Key) -> 
     riakc_cluster:get(ProcessName, Table, Key, [{r, 2}]).
 
--spec delete(_BackendName :: term(), ProcessName :: process_name(), Table :: table(), Key :: key()) -> error() | 'ok'.
+-spec delete(_BackendName :: term(), ProcessName :: cluster_name(), Table :: table(), Key :: key()) -> error() | 'ok'.
 delete(_BackendName, ProcessName, Table, Key) ->
     riakc_cluster:delete(ProcessName, Table, Key, [{rw, 2}]).
 
--spec list_keys(_BackendName :: term(), ProcessName :: process_name(), Table :: table()) -> error() | {'ok', [key()]}.
+-spec list_keys(_BackendName :: term(), ProcessName :: cluster_name(), Table :: table()) -> error() | {'ok', [key()]}.
 list_keys(_BackendName, ProcessName, Table) ->
     riakc_cluster:list_keys(ProcessName, Table).
 
--spec list_buckets(_BackendName :: term(), ProcessName :: process_name()) -> error() | {'ok', [table()]}.
+-spec list_buckets(_BackendName :: term(), ProcessName :: cluster_name()) -> error() | {'ok', [table()]}.
 list_buckets(_BackendName, ProcessName) ->
     riakc_cluster:list_buckets(ProcessName).
